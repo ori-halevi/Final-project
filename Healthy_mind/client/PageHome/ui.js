0cc7aaaf59971695128a338117c82831834f0e3a
ori halevi <orihalevi98@gmail.com>
2024-06-17 20:27:27 +0300

    Ori

--- Healthy_mind/client/PageHome/ui.js
+++ Healthy_mind/client/PageHome/ui.js
@@ -1,16 +1,28 @@
+const doctorsNamesDropdownSuggestionsDiv = document.getElementById(
+  "doctors-names-dropdown-suggestions-div"
+);
+const doctorsNamesList = document.getElementById("doctors-names-list");
+const doctorsNamesDropdownButton = document.getElementById(
+  "doctors-names-dropdown-button"
+);
+const doctorsNamesSearchBoxInput = document.getElementById(
+  "doctors-names-searchBox-input"
+);
+const doctorsNamesDropdownMainDiv = document.getElementById(
+  "doctors-names-dropdown-main-div"
+);
 
-const doctorsNamesDropdownSuggestionsDiv = document.getElementById("doctors-names-dropdown-suggestions-div")
-const doctorsNamesList = document.getElementById("doctors-names-list")
-const doctorsNamesDropdownButton = document.getElementById("doctors-names-dropdown-button")
-const doctorsNamesSearchBoxInput = document.getElementById("doctors-names-searchBox-input")
 
+
+// START OF DOCTORS DROPDOWN BUTTON
+
+// By ori, this is the function that is called when the user enters a character in the search box.
 doctorsNamesSearchBoxInput.addEventListener("input", function () {
   let filter = this.value.toLowerCase();
-  let doctorsNamesList = document.getElementById("doctors-names-list");
   let doctors = doctorsNamesList.getElementsByTagName("li");
 
   for (let i = 0; i < doctors.length; i++) {
-    let doctorName = doctors[i].textContent || doctors[i].innerText;
+    let doctorName = doctors[i].textContent;
     if (doctorName.toLowerCase().indexOf(filter) > -1) {
       doctors[i].style.display = "";
     } else {
@@ -19,24 +31,47 @@ doctorsNamesSearchBoxInput.addEventListener("input", function () {
   }
 });
 
+// By ori, this is the function that is called when the user clicks on a doctor name in the list.
 doctorsNamesList.addEventListener("click", function (e) {
   if (e.target && e.target.nodeName == "LI") {
     document.getElementById("doctors-names-dropdown-button").textContent =
       e.target.textContent;
-      doctorsNamesDropdownSuggestionsDiv.style.display = "none";
+    doctorsNamesDropdownSuggestionsDiv.style.display = "none";
   }
 });
 
+// By ori, this is the function that is called when the user clicks on the dropdown button.
 doctorsNamesDropdownButton.addEventListener("click", function () {
-    if (doctorsNamesDropdownSuggestionsDiv.style.display === "block") {
-      doctorsNamesDropdownSuggestionsDiv.style.display = "none";
-    } else {
-      doctorsNamesDropdownSuggestionsDiv.style.display = "block";
-    }
-  });
+  if (doctorsNamesDropdownSuggestionsDiv.style.display === "block") {
+    doctorsNamesDropdownSuggestionsDiv.style.display = "none";
+  } else {
+    doctorsNamesDropdownSuggestionsDiv.style.display = "block";
+  }
+});
 
+// END OF DOCTORS DROPDOWN BUTTON
+
+
+// START OF CATEGORIES DROPDOWN BUTTON
+
+
+
+
+// END OF CATEGORIES DROPDOWN BUTTON
+
+
+
+
+
+
+
+
+
+
+// By ori, this is the function that is called when the user clicks anywhere in the document.
 document.addEventListener("click", function (e) {
-  if (!document.getElementById("doctors-names-dropdown-main-div").contains(e.target)) {
+  // This ensures that when you click somewhere outside the list box or the box button, the list will close.
+  if (!doctorsNamesDropdownMainDiv.contains(e.target)) {
     doctorsNamesDropdownSuggestionsDiv.style.display = "none";
   }
 });
