7046b9ff080e83fc772e3f57537ebcd88e1940b9
ElkanaRozental <rozentlen@gmail.com>
2024-06-18 16:30:28 +0300

    elkana

--- Healthy_mind/client/PageLogin/app.js
+++ Healthy_mind/client/PageLogin/app.js
@@ -1,25 +1,42 @@
-async function fetchUserInfo() {
-    try {
-      const response = await fetch("http://localhost:8080/api/getUserInfo");
-      const user = await response.json();
-      return user;
-    } catch (error) {
-      console.error("Error fetching users info:", error);
-      return [];
-    }
-  }
-
 const password = document.getElementById("password")
 const username = document.getElementById("username")
 const login = document.getElementById("logBtn")
 
+async function fetchUserInfo(username, password) {
+    try {
+      const response = await fetch("http://localhost:8080/api/isUserExists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username}, {password}),
+      });
+        console.log(response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+  
+      const data = await response.json();
+      // console.log("User Info:", data);
+      // כאן תוכל להשתמש במידע שהתקבל על הרופא
+      return data;
+    } catch (error) {
+      console.error("Fetch failed:", error);
+      // טיפול בשגיאה
+    }
+  }
 
-function userCheck(username, password) {
-    if(username === fetchUserInfo.full_name && password === fetchUserInfo.password){
-       login.onclick = window.location.href="./PageHome/index.html"
+const order = login.onclick
+function enter(order) {
+    if(order){
+        window.location.href="../PageHome/index.html"
     }
-    else{
-        return
-    }
-}
-userCheck(username,password)    
+} 
+
+
+console.log("hello")
+
+fetchUserInfo(ori, 11233)
+
+
+
