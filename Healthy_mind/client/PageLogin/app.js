894afdc7b3fe33b2eef015e3b2b3a69b21583c84
Eliyaou52 <e9189891@gmail.com>
2024-06-18 21:40:21 +0300

    eliy

--- Healthy_mind/client/PageLogin/app.js
+++ Healthy_mind/client/PageLogin/app.js
@@ -1,42 +1,35 @@
-const password = document.getElementById("password")
-const username = document.getElementById("username")
-const login = document.getElementById("logBtn")
+const password = document.getElementById("password");
+const username = document.getElementById("username");
+const login = document.getElementById("logBtn");
 
 async function fetchUserInfo(username, password) {
-    try {
-      const response = await fetch("http://localhost:8080/api/isUserExists", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({username}, {password}),
-      });
-        console.log(response);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-  
-      const data = await response.json();
-      // console.log("User Info:", data);
-      // כאן תוכל להשתמש במידע שהתקבל על הרופא
-      return data;
-    } catch (error) {
-      console.error("Fetch failed:", error);
-      // טיפול בשגיאה
+  try {
+    const response = await fetch("http://localhost:8080/api/isUserExists", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username }, { password }),
+    });
+    console.log(response);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+
+    const data = await response.json();
+    // console.log("User Info:", data);
+    return data;
+  } catch (error) {
+    console.error("Fetch failed:", error);
   }
+}
 
-const order = login.onclick
-function enter(order) {
-    if(order){
-        window.location.href="../PageHome/index.html"
-    }
-} 
-
-
-console.log("hello")
-
-fetchUserInfo(ori, 11233)
+// const order = login.onclick;
+login.addEventListener("click", () => {
+  window.location.href = `../PageHome/index.html?userId=${'test'}`;
+});
 
 
+console.log("hello");
 
+fetchUserInfo(ori, 11233);
