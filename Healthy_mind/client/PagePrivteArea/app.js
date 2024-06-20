4df387429856ea18af1bd4ea0281073a0aa0316a
Eliyaou52 <e9189891@gmail.com>
2024-06-20 17:00:00 +0300

    eli

--- Healthy_mind/client/PagePrivteArea/app.js
+++ Healthy_mind/client/PagePrivteArea/app.js
@@ -1,3 +1,28 @@
+const { log } = require("console");
+
+
+function getQueryParams() {
+  const params = {};
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  for (const [key, value] of urlParams) {
+    params[key] = value;
+    // console.log("key: " + key + " value: " + value);
+  }
+  return params;
+}
+
+async function getPageUserId() {
+  const params = await getQueryParams();
+  if (!(params.userId === null)) {
+    return params.userId;
+  } else {
+    console.error("User ID not found in URL parameters");
+    return null;
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const appointments = [
       {
@@ -22,3 +47,26 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
+  async function getUserInformationById(userId){
+    try {
+      const response = await fetch("http://localhost:8080/api/getUserInfoById", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Fetch failed:", error);
+    }
+  }
+
+async function sendInfoUser(userid){
+  const info = getUserInformationById(userid)
+  console.log('test');
+}
+sendInfoUser(getPageUserId())
